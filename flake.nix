c70a7f812ef221b8927ce548b16cbe663eb90b7b
uttarayan21 <email@uttarayan.me>
2025-04-13 01:14:55 +0530

    feat: Added nice_view

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    zmk-nix,
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+  in {
+    packages = forAllSystems (system: rec {
+      default = firmware;
+
+      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+        name = "firmware";
+
+        src = nixpkgs.lib.sourceFilesBySuffices self [".board" ".cmake" ".conf" ".defconfig" ".dts" ".dtsi" ".json" ".keymap" ".overlay" ".shield" ".yml" "_defconfig"];
+
+        board = "nice_nano_v2";
+        shield = "corne_%PART% nice_view";
+
+        zephyrDepsHash = "sha256-CV+AUBetseibTkuB9BJ6R9KLENMz/Or46/+liEjD/6s=";
+
+        meta = {
+          description = "ZMK firmware";
+          license = nixpkgs.lib.licenses.mit;
+          platforms = nixpkgs.lib.platforms.all;
+        };
+      };
+
+      flash = zmk-nix.packages.${system}.flash.override {inherit firmware;};
+      update = zmk-nix.packages.${system}.update;
+    });
+
+    devShells = forAllSystems (system: {
+      default = zmk-nix.devShells.${system}.default;
+    });
+  };
+}

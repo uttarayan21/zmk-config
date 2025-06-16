643dfbc594a600aaacbbaaa901ad9b9e02970580
uttarayan21 <email@uttarayan.me>
2025-06-17 01:48:10 +0530

    feat: I give up switching to rmk

--- flake.nix
+++ flake.nix
@@ -22,22 +22,39 @@
       platforms = nixpkgs.lib.platforms.all;
     };
   in {
-    packages = forAllSystems (system: rec {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in rec {
       default = corne;
 
       corne = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
         inherit src zephyrDepsHash meta;
         name = "corne";
         board = "nice_nano_v2";
+        parts = ["dongle" "left" "right"];
         shield = "corne_%PART% nice_view_adapter nice_view";
-        extraCmakeFlags = ["-DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n"];
+        extraCmakeFlags = [
+          "-DCONFIG_ZMK_SPLIT_ROLE_CENTRAL=n"
+          "-DCMAKE_C_FLAGS=-Wno-int-conversion"
+        ];
       };
-      corne-dongle = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
+      corne-just-dongle = zmk-nix.legacyPackages.${system}.buildKeyboard {
+        inherit src zephyrDepsHash meta;
+        name = "corne";
+        board = "nice_nano_v2";
+        shield = "corne_dongle";
+        extraCmakeFlags = ["-DCMAKE_C_FLAGS=-Wno-int-conversion"];
+      };
+      # corne-with-dongle = pkgs.symlinkJoin {
+      #   name = "corne-with-dongle";
+      #   paths = [corne corne-just-dongle];
+      # };
+      corne-with-dongle = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
         inherit src zephyrDepsHash meta;
         name = "corne";
         board = "nice_nano_v2";
         parts = ["dongle" "left" "right"];
-        shield = "corne_%PART% nice_view_adapter nice_view";
+        shield = "corne_%PART% nice_view_adapter nice_epaper";
         extraCmakeFlags = ["-DCMAKE_C_FLAGS=-Wno-int-conversion"];
       };
       reset = zmk-nix.legacyPackages.${system}.buildKeyboard {
